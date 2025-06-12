6fabca3960d30f7dfea9068f6ca1b89e44f0db27
FiokL <160193244+FiokL@users.noreply.github.com>
2025-06-13 04:47:01 +0500

    hw08.rs

---
+++ src/homework/hw08.rs
@@ -0,0 +1,53 @@
+fn is_prime(n: &u32) -> bool {
+    let num = *n;
+    
+    if num <= 1 {
+        return false;
+    }
+    
+    if num == 2 {
+        return true;
+    }
+    
+    if num % 2 == 0 {
+        return false;
+    }
+    
+    let sqrt_num = (num as f64).sqrt() as u32 + 1;
+    for i in (3..sqrt_num).step_by(2) {
+        if num % i == 0 {
+            return false;
+        }
+    }
+    
+    true
+}
+
+fn test_is_prime() {
+    let test_data = [
+        (0, false),
+        (1, false),
+        (2, true),
+        (3, true),
+        (4, false),
+        (5, true),
+        (100, false),
+        (10007, true),
+    ];
+
+    test_data
+        .iter()
+        .for_each(|(n, prime)| 
+            assert_eq!(is_prime(n), *prime)
+        );
+    println!("All tests passed!");
+}
+
+fn main() {
+    let numbers = [7, 10, 13, 20];
+    for n in numbers.iter() {
+        println!("{} is prime? {}", n, is_prime(n));
+    }
+    
+    test_is_prime();
+}
